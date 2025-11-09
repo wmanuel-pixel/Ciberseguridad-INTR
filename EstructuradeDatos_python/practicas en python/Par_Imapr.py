7b22efa74bd0fd73e5e0c7cb86665605921af1ba
wmanuel-pixel <wmanuel967@gmail.com>
2025-11-09 19:09:58 -0400

    test

---
+++ EstructuradeDatos_python/practicas en python/Par_Imapr.py
@@ -0,0 +1,7 @@
+# Determina si un número es par o impar
+numero = int(input("Ingrese un número entero: "))
+
+if numero % 2 == 0:
+    print(f"El número {numero} es **par**.")
+else:
+    print(f"El número {numero} es **impar**.")
