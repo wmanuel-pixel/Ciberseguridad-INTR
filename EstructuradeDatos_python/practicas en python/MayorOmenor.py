ccf15512239a48a649513b029fd1179f45e6b10a
wmanuel-pixel <wmanuel967@gmail.com>
2025-11-09 19:02:00 -0400

    test

---
+++ EstructuradeDatos_python/practicas en python/MayorOmenor.py
@@ -0,0 +1,7 @@
+print("Programa para determinar si una persona es mayor o menor de edad.")
+edad = int(input("Ingrese su edad: "))
+
+if edad >= 18:
+    print("Usted es **mayor de edad**.")
+else:
+    print("Usted es **menor de edad**.")
