896a7ad62ce448d94dbfca0ade63a2906b567234
wmanuel-pixel <wmanuel967@gmail.com>
2025-11-10 09:47:48 -0400

    test

--- PromedioDeNotas.py
+++
@@ -1,8 +0,0 @@
-# Definir la nota de aprobación
-nota_aprobacion = 70.0
-nota = float(input("Ingrese la nota del estudiante: "))
-
-if nota >= nota_aprobacion:
-    print("El estudiante **aprueba**.")
-else:
-    print("El estudiante **reprueba**.")
